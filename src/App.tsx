f708a9d3f5166f89bc0ce7733d63a98beb4ab1f8
wramadan7-org <wramadan1203@gmail.com>
2024-03-14 15:49:04 +0700

    fix: validation form using formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 interface InterfaceBookingTicket {
   airline: string;
@@ -6,9 +10,23 @@ interface InterfaceBookingTicket {
   code_booking: string;
   first_name: string;
   last_name: string;
+  date: Date | null;
   capture?: unknown;
   agree: boolean;
 }
+
+const validationSchema = Yup.object().shape({
+  airline: Yup.string().required("Maskapai harus diisi"),
+  flight_number: Yup.string().required("Nomor penerbangan harus diisi"),
+  code_booking: Yup.string().required("Kode pemesanan harus diisi"),
+  first_name: Yup.string().required("Nama depan harus diisi"),
+  last_name: Yup.string().required("Nama belakang harus diisi"),
+  date: Yup.date().required("Tanggal penerbangan harus diisi"),
+  agree: Yup.boolean().oneOf(
+    [true],
+    "Anda harus menyetujui syarat dan ketentuan"
+  ),
+});
 
 const App = () => {
   const initialBookingTicket = {
@@ -17,8 +35,11 @@ const App = () => {
     code_booking: "",
     first_name: "",
     last_name: "",
+    date: new Date() || null,
     agree: false,
   };
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
 
   const [formBookingTicketState, setFormBookingTicketState] =
     useState<InterfaceBookingTicket>(initialBookingTicket);
@@ -69,35 +90,6 @@ const App = () => {
     }
   };
 
-  const handleChangeFormBookingTicket = (
-    event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
-    const { name, value } = event.target;
-
-    setFormBookingTicketState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const handleChangeFormBookingTicketChecked = (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    setFormBookingTicketState((prevState) => ({
-      ...prevState,
-      agree: event?.target?.checked,
-    }));
-  };
-
-  const handleSubmitFormBookingTicket = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = {
-      ...formBookingTicketState,
-      capture: imageDataUrl,
-    };
-    console.log("SUBMIT: ", data);
-  };
-
   return (
     <div className="flex flex-1 flex-col w-[100vw] h-[100vh] items-center justify-start bg-white overflow-y-scroll pb-5">
       <div className="max-h-[50px] w-full bg-[#be0c18] text-white font-bold text-lg text-center py-2">
@@ -110,198 +102,274 @@ const App = () => {
           Tambahkan Penerbangan Saya
         </span>
 
-        <form
-          className="gap-5 flex flex-col"
-          onSubmit={handleSubmitFormBookingTicket}
+        <Formik
+          initialValues={{
+            airline: "",
+            flight_number: "",
+            code_booking: "",
+            first_name: "",
+            last_name: "",
+            date: new Date(),
+            agree: false,
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            console.log("VALUES: ", values);
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }, 500);
+          }}
         >
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="capture">
-              Capture
-            </label>
+          {(formik) => (
+            <Form className="gap-5 flex flex-col">
+              {/* Your form content here */}
 
-            <div className="flex justify-center gap-2">
-              <button
-                type="button"
-                onClick={handleOpenCamera}
-                className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
-              >
-                Buka Kamera
-              </button>
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="capture">
+                  Capture
+                </label>
 
-              <button
-                type="button"
-                onClick={handleCapture}
-                className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
-              >
-                Ambil Foto
-              </button>
-            </div>
+                <div className="flex justify-center gap-2">
+                  <button
+                    type="button"
+                    onClick={handleOpenCamera}
+                    className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
+                  >
+                    Buka Kamera
+                  </button>
 
-            {imageDataUrl && (
-              <div
-                className="flex max-h-56 justify-center"
-                style={{ display: showCameraState ? "none" : "" }}
-              >
-                <img src={imageDataUrl} className="rounded-md" alt="Captured" />
+                  <button
+                    type="button"
+                    onClick={handleCapture}
+                    className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
+                  >
+                    Ambil Foto
+                  </button>
+                </div>
+
+                {imageDataUrl && (
+                  <div
+                    className="flex justify-center mt-10"
+                    style={{
+                      display: showCameraState ? "none" : "",
+                      height: "50%",
+                    }}
+                  >
+                    <img
+                      src={imageDataUrl}
+                      className="rounded-md"
+                      alt="Captured"
+                    />
+                  </div>
+                )}
+
+                <div className="flex flex-1 w-full h-40 items-center justify-center">
+                  <video
+                    ref={videoRef}
+                    autoPlay
+                    playsInline
+                    style={{
+                      maxWidth: "50%",
+                      width: "100%",
+                      display: showCameraState ? "" : "none",
+                    }}
+                  />
+                </div>
               </div>
-            )}
 
-            <div className="flex flex-1 w-full h-40 items-center justify-center">
-              <video
-                ref={videoRef}
-                autoPlay
-                playsInline
-                style={{
-                  maxWidth: "30%",
-                  width: "100%",
-                  display: showCameraState ? "" : "none",
-                }}
+              {/* Maskapai */}
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="airline">
+                  Maskapai
+                </label>
+
+                <div className="border flex flex-row bg-white rounded-full h-10 px-3 items-center">
+                  <span className="text-sm uppercase">ID -</span>
+
+                  <Field
+                    as="select"
+                    id="airline"
+                    name="airline"
+                    className="bg-white flex flex-1 rounded-r-full w-full outline-none focus:outline-none text-sm "
+                    onChange={formik.handleChange}
+                  >
+                    <option className="text-sm" value="">
+                      Pilih Maskapai
+                    </option>
+                    <option className="text-sm" value="Garuda Indonesia">
+                      Garuda Indonesia
+                    </option>
+                    <option className="text-sm" value="Citilink">
+                      Citilink
+                    </option>
+                    <option className="text-sm" value="Lion Air">
+                      Lion Air
+                    </option>
+                    <option className="text-sm" value="Batik Air">
+                      Batik Air
+                    </option>
+                  </Field>
+                </div>
+
+                <ErrorMessage
+                  name="airline"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              {/* Nomor Penerbangan */}
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="flight_number">
+                  Nomor Penerbangan
+                </label>
+
+                <div className="border flex flex-row bg-white rounded-full px-3 h-10 items-center">
+                  <span className="border-r pr-2 h-full text-center flex items-center text-sm uppercase">
+                    ID
+                  </span>
+
+                  <Field
+                    id="flight_number"
+                    name="flight_number"
+                    type="text"
+                    className="bg-white flex flex-1 rounded-r-full w-full px-2 outline-none focus:outline-none text-sm"
+                    placeholder="Nomor Penerbangan"
+                    onChange={formik.handleChange}
+                  />
+                </div>
+
+                <ErrorMessage
+                  name="flight_number"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              {/* Kode Pemesanan */}
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="code_booking">
+                  Kode Pemesanan
+                </label>
+
+                <Field
+                  id="code_booking"
+                  name="code_booking"
+                  type="text"
+                  placeholder="Kode Pemesanan"
+                  className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border text-sm"
+                  onChange={formik.handleChange}
+                />
+
+                <ErrorMessage
+                  name="code_booking"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="first_name">
+                  Nama Depan
+                </label>
+
+                <Field
+                  id="first_name"
+                  name="first_name"
+                  type="text"
+                  placeholder="Nama Depan"
+                  className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border uppercase text-xs"
+                  onChange={formik.handleChange}
+                />
+
+                <ErrorMessage
+                  name="first_name"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label hidden htmlFor="last_name">
+                  Nama Belakang
+                </label>
+
+                <Field
+                  id="last_name"
+                  name="last_name"
+                  type="text"
+                  placeholder="Nama Belakang"
+                  className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border uppercase text-xs"
+                  onChange={formik.handleChange}
+                />
+
+                <ErrorMessage
+                  name="last_name"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <label hidden htmlFor="date">
+                  Date
+                </label>
+
+                <DatePicker
+                  id="date"
+                  name="date"
+                  selected={formBookingTicketState?.date}
+                  onChange={(event) =>
+                    setFormBookingTicketState((prevState) => ({
+                      ...prevState,
+                      date: event,
+                    }))
+                  }
+                  className="h-10 w-full rounded-full outline-none focus:outline-none px-3 bg-white border uppercase text-xs"
+                  minDate={new Date()}
+                  maxDate={tomorrow}
+                />
+
+                <ErrorMessage
+                  name="date"
+                  component="div"
+                  className="text-red-500 text-xs"
+                />
+              </div>
+
+              <div className="flex flex-row gap-2 items-center">
+                <Field
+                  id="agree"
+                  name="agree"
+                  type="checkbox"
+                  className="checked:text-[#d50f24] text-white"
+                  onChange={formik.handleChange}
+                />
+
+                <span className="text-xs mb-1 font-semibold">
+                  Saya telah membaca dan menyetujui{" "}
+                  <span className="text-[#d50f24] font-bold">
+                    Syarat dan Ketentuan
+                  </span>
+                </span>
+              </div>
+
+              <ErrorMessage
+                name="agree"
+                component="div"
+                className="text-red-500 text-xs"
               />
-            </div>
-          </div>
 
-          {/* Maskapai */}
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="airline">
-              Maskapai
-            </label>
-
-            <div className="border flex flex-row bg-white rounded-full h-10 px-3 items-center">
-              <span className="text-sm uppercase">ID -</span>
-
-              <select
-                id="airline"
-                name="airline"
-                className="bg-white flex flex-1 rounded-r-full w-full outline-none focus:outline-none text-sm "
-                value={formBookingTicketState?.airline}
-                onChange={(event: ChangeEvent<HTMLSelectElement>) =>
-                  handleChangeFormBookingTicket(event)
-                }
+              <button
+                type="submit"
+                className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
               >
-                <option className="text-sm" value="">
-                  Pilih Maskapai
-                </option>
-                <option className="text-sm" value="Garuda Indonesia">
-                  Garuda Indonesia
-                </option>
-                <option className="text-sm" value="Citilink">
-                  Citilink
-                </option>
-                <option className="text-sm" value="Lion Air">
-                  Lion Air
-                </option>
-                <option className="text-sm" value="Batik Air">
-                  Batik Air
-                </option>
-              </select>
-            </div>
-          </div>
-
-          {/* Nomor Penerbangan */}
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="flight_number">
-              Nomor Penerbangan
-            </label>
-
-            <div className="border flex flex-row bg-white rounded-full px-3 h-10 items-center">
-              <span className="border-r pr-2 h-full text-center flex items-center text-sm uppercase">
-                ID
-              </span>
-
-              <input
-                id="flight_number"
-                name="flight_number"
-                type="text"
-                className="bg-white flex flex-1 rounded-r-full w-full px-2 outline-none focus:outline-none text-sm"
-                placeholder="Nomor Penerbangan"
-                value={formBookingTicketState?.flight_number}
-                onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                  handleChangeFormBookingTicket(event)
-                }
-              />
-            </div>
-          </div>
-
-          {/* Kode Pemesanan */}
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="code_booking">
-              Kode Pemesanan
-            </label>
-
-            <input
-              id="code_booking"
-              name="code_booking"
-              type="text"
-              placeholder="Kode Pemesanan"
-              className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border text-sm"
-              value={formBookingTicketState?.code_booking}
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                handleChangeFormBookingTicket(event)
-              }
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="first_name">
-              Nama Depan
-            </label>
-
-            <input
-              id="first_name"
-              name="first_name"
-              type="text"
-              placeholder="Nama Depan"
-              className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border uppercase text-xs"
-              value={formBookingTicketState?.first_name}
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                handleChangeFormBookingTicket(event)
-              }
-            />
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label hidden htmlFor="last_name">
-              Nama Depan
-            </label>
-
-            <input
-              id="last_name"
-              name="last_name"
-              type="text"
-              placeholder="Nama Belakang"
-              className="h-10 rounded-full outline-none focus:outline-none px-3 bg-white border uppercase text-xs"
-              value={formBookingTicketState?.last_name}
-              onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                handleChangeFormBookingTicket(event)
-              }
-            />
-          </div>
-
-          <div className="flex flex-row gap-2 items-center">
-            <input
-              id="agree"
-              name="agree"
-              type="checkbox"
-              className="checked:text-[#d50f24] text-white"
-              checked={formBookingTicketState?.agree}
-              onChange={handleChangeFormBookingTicketChecked}
-            />
-
-            <span className="text-xs mb-1 font-semibold">
-              Saya telah membaca dan menyetujui{" "}
-              <span className="text-[#d50f24] font-bold">
-                Syarat dan Ketentuan
-              </span>
-            </span>
-          </div>
-
-          <button
-            type="submit"
-            className="bg-[#cc1124] text-white text-sm h-10 w-full outline-none hover:bg-[#f2162c] rounded-full hover:outline-none hover:border-none"
-          >
-            Kirim
-          </button>
-        </form>
+                Kirim
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
